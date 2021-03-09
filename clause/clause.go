ff6658a34494b09a2915c1e24452ab1e6646625d
secret42 <secret42@qq.com>
2021-03-09 22:12:18 +0800

    添加语句构造功能

---
+++ clause/clause.go
@@ -0,0 +1,48 @@
+package clause
+
+import "strings"
+
+type Clause struct {
+	sql     map[Type]string
+	sqlVars map[Type][]interface{}
+}
+
+type Type int
+
+const (
+	INSERT Type = iota
+	VALUES
+	SELECT
+	LIMIT
+	WHERE
+	ORDERBY
+)
+
+/*
+添加询问条件
+example:
+clause.Set(LIMIT,3)
+clause.Set(WHERE,"Name = ?","Tom")
+*/
+func (c *Clause) Set(name Type, vars ...interface{}) {
+	if c.sql == nil {
+		c.sql = make(map[Type]string)
+		c.sqlVars = make(map[Type][]interface{})
+	}
+	sql, vars := generators[name](vars...)
+	c.sql[name] = sql
+	c.sqlVars[name] = vars
+}
+
+// sql语句拼接
+func (c *Clause) Build(orders ...Type) (string, []interface{}) {
+	var sqls []string
+	var vars []interface{}
+	for _, order := range orders {
+		if sql, ok := c.sql[order]; ok {
+			sqls = append(sqls, sql)
+			vars = append(vars, c.sqlVars[order]...)
+		}
+	}
+	return strings.Join(sqls, " "), vars
+}
